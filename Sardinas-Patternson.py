c27ec46f0ab009ae30957d9efb4503490b5fcba5
pierfrancescomartinello <pierfrancesco.martinello@outlook.it>
2023-05-24 00:05:37 +0200

    The algorithm seems working. The main function with the input part needs to be added. Documentation needs to be added. Tests need to be done.

--- Sardinas-Patternson.py
+++ Sardinas-Patternson.py
@@ -1,35 +1,51 @@
+## TO BE CHECKED
+import functools
+
 #Given an input we specify, return if the code is UD or not, if it is unbound or not and what is the bound and check whether the possible codifications are.
-def sardinas_patterson(s0):
+def sardinas_patterson(C):
     sets = []
-    set = []
+    temp_set = []
     
-    for i in range(1, len(s0)):
-        for j in range(i, len(s0)):
-            if i.startswith(j) :
-                set.append(i.removeprefix(j))
-    sets.append(set)
-    current_set = set
-    set.clear()
+    #First Iteration: Creation of S1 (saved into current set)
+    for i in C:
+        for j in range(i, len(C)):
+            if i.startswith(C[j]) :
+                temp_set.append(i.removeprefix(j))
+    current_set = temp_set
+    temp_set.clear()
 
-    for i in s0:
-        for j in current_set:
-            if i.startswith(j) :
-                set.append(i.removeprefix(j))
-            if j.startswith(i) :
-                set.append(j.removeprefix(i))
-   
-    current_set = set
-    set.clear() 
-    check, value = check(sets, current_set)
-    sets.append(set)
-    pass
+    while check(code= C, sets=sets, current_set=current_set) not in [0, 1]:
+        sets.append(current_set)
+        current_set.clear()
 
-def check(sets, current_set):
+        for i in C:
+            for j in current_set:
+                if i.startswith(j):
+                    temp_set.append(i.removeprefix(j))
+                if j.startswith(i):
+                    temp_set.append(j.removeprefix(i))
     
-    pass
+        current_set = temp_set
+        temp_set.clear()
+    print(check(code= C, sets= sets, current_set=current_set))
+
+def check(code, sets, current_set):
+    if sets.isEmpty(): return -1
+    if intersection(code, current_set).isEmpty(): return 0
+    for i in sets:
+        if are_equal(i, current_set) or i.isEmpty(): return 1
+    
+    return 2
 
 def kmm(d, lengths):
     sum = 0 
     for l in lengths:
         sum += pow(d, -l)
     return sum if (sum <= 1) else -1
+
+def intersection(list1, list2):
+    return [value for value in list1 if value in list2]
+
+
+def are_equal(list1, list2):
+    return True if functools.reduce(lambda i, j: i and j, map(lambda m, k: m == k, test_list1, test_list2), True) else False
